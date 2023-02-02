a5f8d1329705565ba9ee5556c74d5a12b47bad62
Tag Nguyen <huyqthp03@gmail.com>
2023-02-02 14:03:06 +0700

    test state

--- src/App.js
+++ src/App.js
@@ -3,29 +3,40 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/Mycomponent';
+//de viet 1 class component
+import React from 'react';
 //import la moi chi goi 
 //import 1 dau "." co nghia la dang o thu muc hiện tại(cùng cấp thư mục);".." ám chỉ thư mục cha
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
-
-  return (
-    <div>
+class App extends React.Component
+{
+  render()
+  {
+    return (
+<div>
       hello bro  Ga qua
       <MyComponent></MyComponent>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       hello bro wqeegbkhqwegdakhrdfbvrqwkhedbg
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+    );
+  }
 }
 
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+    
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           hello bro wqeegbkhqwegdakhrdfbvrqwkhedbg
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 export default App;
--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -1,16 +1,23 @@
+//Diem khac biet giua component va html la doi voi component co the ket hop html vs js bang cu phap  JSX;de in ra bien JS thi bat dau {}
 // class component
 // function component(react 16.8 ra đời giúp cho function component có tác dụng giống class component)
 // ly do co the import React tu thu vien react vi khi cai dat da cai dat react roi
 import React from 'react';
 //ke thua lai tat ca cac tinh nang 1 component co the co
 class MyComponent extends React.Component
-{
+{   //dinh nghia 1 bien state co 2 cach (state la 1 bien obj), kiem soat data của component
+    state ={
+        name:'Huy',
+        address: 'HP',
+    };
     //render la tap hop cac khoi html de chuyen no thanh giao dien
     //JSX la 1 cu phap dac biet cua JS (JSX cho phep viet code cua JS trong HTML)
     //JSX chi co the return ve 1 phan tu(1 element)
   render(){
     return (
-        <div>my first component</div>
+        <div>my first component
+            My name is {this.state.name}
+        </div>
     );
   }
 }
